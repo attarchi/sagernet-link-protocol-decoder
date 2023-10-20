b2e915f8c031a50da3a04b5a5305897184eb0305
ryan @archi <attarchi@me.com>
2023-10-20 05:24:25 +0300

    Porting parseUniversal function from java

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,11 @@
-
+"use strict";
 var zlib = require('zlib');
-
-const link = "sn://ssh?eNpjYGBgMDQy1zMAwo1iQE5Rfv4XRrCgsYnptpKMVIWC0qSczGSF7NSfIPGS1OIS3eLiDN2CovyKSt3yzBIgM7G4uDy_6AlQmgEAVTEXww"
-
-console.log(`The test config is ${link}`)
-
-const validate = (link: string) => link.indexOf("sn://") === 0;
-
+const link = "sn://ssh?eNpjYGBgMDQy1zMAwo1iQE5Rfv4XRrCgsYnptpKMVIWC0qSczGSF7NSfIPGS1OIS3eLiDN2CovyKSt3yzBIgM7G4uDy_6AlQmgEAVTEXww";
+console.log(`The test config is ${link}`);
+const validate = (link) => link.indexOf("sn://") === 0;
 if (!validate(link)) {
     throw new Error("Link is not valid");
 }
-
 // Decode the link
 // https://github.com/SagerNet/SagerNet/blob/70e684bae81d4bb4203e860ab88c4319e88f944d/app/src/main/java/io/nekohasekai/sagernet/fmt/UniversalFmt.kt#L28
 // fun parseUniversal(link: String): AbstractBean {
@@ -26,23 +21,20 @@ if (!validate(link)) {
 //         }.requireBean()
 //     }
 // }
-
-const decodeBase64 = (value: string) => Buffer.from(value.replace(/\-/g, '+').replace(/\_/g, '/'), 'base64') as Buffer;
-
-const parseUniversal = (link: string) => {
+const decodeBase64 = (value) => Buffer.from(value.replace(/\-/g, '+').replace(/\_/g, '/'), 'base64');
+const parseUniversal = (link) => {
     if (link.indexOf("?") >= 0) {
         const linkType = link.split("sn://")[1].split("?")[0];
         const linkSettings = link.split("?")[1];
-        const buffer = zlib.inflateSync(decodeBase64(linkSettings)) as Buffer;
-
-        return { linkType, buffer }
-    } else {
+        const buffer = zlib.inflateSync(decodeBase64(linkSettings));
+        return { linkType, buffer };
+    }
+    else {
         const linkType = link.split("sn://")[1].split(":")[0];
         const linkSettings = link.split(":")[2];
         const buffer = decodeBase64(linkSettings);
-
-        return { linkType, buffer }
+        return { linkType, buffer };
     }
-}
+};
 const decodedLink = parseUniversal(link);
 console.log(`Decoded Link, Type is: ${decodedLink.linkType}\nSetting is: ${decodedLink.buffer.toString()}`);
